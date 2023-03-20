128b3f057da9a6c3b4141c4a5639be70293b76f6
GabeByk <gabriel.j.byk@gmail.com>
2023-03-20 16:16:14 -0400

    Initial Commit

---
+++ CS410 Final Project/CS410_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS410_Final_ProjectApp.swift
+//  CS410 Final Project
+//
+//  Created by Gabe on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CS410_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
